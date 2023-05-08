07b7965916fd3b3f54f10a1e4c1f5a886182f9e2
felismao <81837957+felismao@users.noreply.github.com>
2023-05-08 13:55:59 +1000

    Minor change

--- templates/CustomerView/privacypolicy.php
+++ templates/CustomerView/privacypolicy.php
@@ -8,115 +8,15 @@
     <div class="row justify-content-center">
         <div class="col-lg-12 col-md-12 align-content-center">
         <h2><b>Privacy Policy</h2>
-        <p>Primal People is committed to providing quality services to you and this policy outlines our ongoing obligations to you in respect of how we manage your Personal Information.</p>
-        <p>We have adopted the Australian Privacy Principles (APPs) contained in the Privacy Act 1988 (Cth) (the Privacy Act). The NPPs govern the way in which we collect, use, disclose, store, secure and dispose of your Personal Information.</p>
-        <p>A copy of the Australian Privacy Principles may be obtained from the website of The Office of the Australian Information Commissioner at <?= $this->Html->link( 'www.oaic.gov.au', 'http://www.oaic.gov.au/', ['target' => '_blank',]); ?> </p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Mauris id interdum orci, sed rhoncus augue. Fusce pretium arcu neque, eu scelerisque orci accumsan non.
+            Nam fringilla non arcu quis faucibus. Fusce urna justo, porta eget commodo id, pharetra non odio. Vivamus rhoncus augue eros,
+            in cursus nibh blandit eu. In quis nisl rhoncus, pellentesque augue vel, scelerisque dolor. Suspendisse posuere varius sem,
+            ac porta enim interdum quis.</p>
     </div>
 </div>
 </div>
 
-<section style="padding-top: 50px"></section>
-
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-lg-12 col-md-12 align-content-center">
-            <h2>What is Personal Information and why do we collect it?</h2>
-            <p>Personal Information is information or an opinion that identifies an individual. Examples of Personal Information we collect include: names, addresses, email addresses, phone and facsimile numbers.</p>
-            <p>This Personal Information is obtained in many ways including interviews, correspondence, by telephone and facsimile, by email, via our website <?= $this->Html->link(
-                    'Primal People',
-                    ['controller' => 'CustomerView', 'action' => 'index'],
-                    ['class' => ''])
-                ?>, from media and publications,
-                from other publicly available sources, from cookies and from third parties. We don’t guarantee website links or policy of authorised third parties.</p>
-            <p>We collect your Personal Information for the primary purpose of providing our services to you, providing information to our clients and marketing.
-                We may also use your Personal Information for secondary purposes closely related to the primary purpose, in circumstances where you would reasonably expect such use or disclosure.
-                You may unsubscribe from our mailing/marketing lists at any time by contacting us in writing.</p>
-            <p>When we collect Personal Information we will, where appropriate and where possible, explain to you why we are collecting the information and how we plan to use it.</p>
-        </div>
-    </div>
-</div>
-
-<section style="padding-top: 50px"></section>
-
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-lg-12 col-md-12 align-content-center">
-        <h2>Sensitive Information</h2>
-        <p>Sensitive information is defined in the Privacy Act to include information or opinion about such things as an individual's racial or ethnic origin, political opinions, membership of a political association, religious or philosophical beliefs, membership of a trade union or other professional body, criminal record or health information.</p>
-        <p>Sensitive information will be used by us only:<br>
-            &emsp;◾ For the primary purpose for which it was obtained <br>
-            &emsp;◾ For a secondary purpose that is directly related to the primary purpose<br>
-            &emsp;◾ With your consent; or where required or authorised by law
-    </div>
-</div>
-</div>
-
-<section style="padding-top: 50px"></section>
-
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-lg-12 col-md-12 align-content-center">
-        <h2>Third Parties</h2>
-        <p>Where reasonable and practicable to do so, we will collect your Personal Information only from you.
-            However, in some circumstances we may be provided with information by third parties.
-            In such a case we will take reasonable steps to ensure that you are made aware of the information provided to us by the third party.</p>
-    </div>
-</div>
-</div>
-
-<section style="padding-top: 50px"></section>
-
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-lg-12 col-md-12 align-content-center">
-        <h2>Disclosure of Personal Information</h2>
-        <p>Your Personal Information may be disclosed in a number of circumstances including the following: <br>
-            &emsp;◾ Third parties where you consent to the use or disclosure; and<br>
-        &emsp;◾ Where required or authorised by law.</p>
-    </div>
-</div>
-</div>
-
-<section style="padding-top: 50px"></section>
-
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-lg-12 col-md-12 align-content-center">
-        <h2>Security of Personal Information</h2>
-        <p>Your Personal Information is stored in a manner that reasonably protects it from misuse and loss and from unauthorized access, modification or disclosure.</p>
-        <p>When your Personal Information is no longer needed for the purpose for which it was obtained, we will take reasonable steps to destroy or permanently de-identify your Personal Information.
-            However, most of the Personal Information is or will be stored in client files which will be kept by us for a minimum of 7 years.</p>
-    </div>
-</div>
-</div>
-
-<section style="padding-top: 50px"></section>
-
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-lg-12 col-md-12 align-content-center">
-        <h2>Accessing, Modifying and/or Up-dating Personal Information</h2>
-        <p>Personal information supplies to Primal People is always available for viewing via our website by members log-in at any time.
-            You may also access the Personal Information we hold about you and to update and/or correct it, subject to certain exceptions.
-            If you wish to access your Personal Information, please contact us via our administration team on 0412 510 424, via our website ‘Contact Form’ or in writing to ‘PO Box 7128, Melbourne Vic 3004’.</p>
-        <p>Primal People will not charge any fee for your access request, but may charge an administrative fee for providing a copy of your Personal Information.</p>
-        <p>In order to protect your Personal Information we may require further identification from you before releasing the requested information.</p>
-    </div>
-</div>
-</div>
-
-<section style="padding-top: 50px"></section>
-
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-lg-12 col-md-12 align-content-center">
-        <h2>Maintaining the Quality of your Personal Information</h2>
-        <p>It is an important to us that your Personal Information is up to date.
-            We will take reasonable steps to make sure that your Personal Information is accurate, complete and up-to-date.
-            If you find that the information we have is not up to date or is inaccurate, please advise us as soon as practicable so we can update our records and ensure we can continue to provide quality services to you.</p>
-    </div>
-</div>
-</div>
 
 <section style="padding-top: 50px"></section>
 
@@ -136,13 +36,13 @@
         <div class="col-lg-12 col-md-12 align-content-center">
         <h2>Privacy Policy Complaints and/or Enquiries</h2>
             <p>If you have any queries or complaints about our Privacy Policy, please contact us at:<br>
-            <b>In writing:</b> PO Box 7128, Melbourne, Vic 3004<br>
+            <b>In writing:</b> Address<br>
                 <b>Via our website:</b> <?= $this->Html->link(
                     'Contact Us',
                     ['controller' => 'Emails', 'action' => 'add'],
                     ['class' => ''])
                 ?><br>
-                <b>Via phone on:</b> 0412 510 424</p>
+                <b>Via phone on:</b> +123 456 789</p>
     </div>
 </div>
 </div>
